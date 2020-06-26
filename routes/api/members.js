c7611ed0c30a30cca7a9f8890ac5da10091e1193
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-26 16:14:22 -0500

    added update Member route

--- routes/api/members.js
+++ routes/api/members.js
@@ -38,7 +38,14 @@ res.json(members);
 router.put('/:id', (req, res) => {
   const found = members.some(member => member.id === parseInt(req.params.id));
   if(found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    const updMember = req.body;
+    members.forEach(member => {
+      if(member.id = parseInt(req.params.id)){
+        member.name = updMember.name? updMember.name : member.name;
+        member.email = updMember.email? updMember.email : member.email;
+        res.json({ msg: 'Member updated', member });
+      }
+    });
   } else {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
   }
